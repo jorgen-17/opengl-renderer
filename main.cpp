c9eb4eb8160bf72d79e0bfb793cce348b1a87052
jorgen-17 <jorgenonelljr@gmail.com>
2026-03-14 23:18:11 -0400

    tutorial10 using index buffer instead of glDrawArray for space efficiency

--- main.cpp
+++ main.cpp
@@ -8,15 +8,12 @@
 #include "ogldev_math_3d.h"
 
 GLuint VBO;
+GLuint IBO;
 GLuint gWorldLocation;
-
-const int numPoints = 3;
-Vector3f Vertices[numPoints];
 
 static void RenderSceneCB()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-
 
     static float Scale = 0.0f;
 
@@ -24,20 +21,19 @@ static void RenderSceneCB()
 
     Matrix4f World;
 
-    World.m[0][0] = sinf(Scale) ; World.m[0][1] = 0.0f       ; World.m[0][2] = 0.0f;        World.m[0][3] = 0.0f;
-    World.m[1][0] = 0.0f        ; World.m[1][1] = sinf(Scale); World.m[1][2] = 0.0f;        World.m[1][3] = 0.0f;
-    World.m[2][0] = 0.0f;       ; World.m[2][1] = 0.0f;      ; World.m[2][2] = sinf(Scale); World.m[2][3] = 0.0f;
-    World.m[3][0] = 0.0f;       ; World.m[3][1] = 0.0f;      ; World.m[3][2] = 0.0f;        World.m[3][3] = 1.0f;
+    World.m[0][0] = cosf(Scale); World.m[0][1] = 0.0f; World.m[0][2] = -sinf(Scale); World.m[0][3] = 0.0f;
+    World.m[1][0] = 0.0;         World.m[1][1] = 1.0f; World.m[1][2] = 0.0f        ; World.m[1][3] = 0.0f;
+    World.m[2][0] = sinf(Scale); World.m[2][1] = 0.0f; World.m[2][2] = cosf(Scale) ; World.m[2][3] = 0.0f;
+    World.m[3][0] = 0.0f;        World.m[3][1] = 0.0f; World.m[3][2] = 0.0f        ; World.m[3][3] = 1.0f;
 
     glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &World.m[0][0]);
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
     glEnableVertexAttribArray(0);
-
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
 
-    glDrawArrays(GL_TRIANGLES, 0, numPoints);
+    glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
 
     glDisableVertexAttribArray(0);
 
@@ -46,15 +42,35 @@ static void RenderSceneCB()
     glutSwapBuffers();
 }
 
+static void InitializeGlutCallbacks()
+{
+    glutDisplayFunc(RenderSceneCB);
+    glutIdleFunc(RenderSceneCB);
+}
+
 static void CreateVertexBuffer()
 {
-    Vertices[0] = Vector3f(-0.5f, -0.5f, 0.0f);
-    Vertices[1] = Vector3f(0.5f, -0.5f, 0.0f);
-    Vertices[2] = Vector3f(0.0f, 0.5f, 0.0f);
+    Vector3f Vertices[4];
+    Vertices[0] = Vector3f(-1.0f, -1.0f, 0.0f);
+    Vertices[1] = Vector3f(0.0f, -1.0f, 1.0f);
+    Vertices[2] = Vector3f(1.0f, -1.0f, 0.0f);
+    Vertices[3] = Vector3f(0.0f, 1.0f, 0.0f);
 
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+}
+
+static void CreateIndexBuffer()
+{
+    unsigned int Indices[] = { 0, 3, 1,
+                               1, 3, 2,
+                               2, 3, 0,
+                               0, 1, 2 };
+
+    glGenBuffers(1, &IBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
 }
 
 static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
@@ -145,15 +161,11 @@ int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH);
-    int width = 2560;
-    int height = 1600;
-    glutInitWindowSize(width, height);
+    glutInitWindowSize(2560, 1600);
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow("Tutorial 10");
 
-    int x = 0;
-    int y = 0;
-    glutInitWindowPosition(x, y);
-    int win = glutCreateWindow("Tutorial 09");
-    printf("window id: %d\n", win);
+    InitializeGlutCallbacks();
 
     // Must be done after glut is initialized!
     GLenum res = glewInit();
@@ -162,14 +174,14 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    GLclampf Red = 0.0f, Green = 0.0f, Blue = 0.0f, Alpha = 0.0f;
-    glClearColor(Red, Green, Blue, Alpha);
+    printf("GL version: %s\n", glGetString(GL_VERSION));
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
     CreateVertexBuffer();
+    CreateIndexBuffer();
 
     CompileShaders();
-
-    glutDisplayFunc(RenderSceneCB);
 
     glutMainLoop();
 

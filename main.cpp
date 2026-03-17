a1a1028da96df934f3dfe873f665cbe54c01328b
jorgen-17 <jorgenonelljr@gmail.com>
2026-03-16 23:49:27 -0400

    tutorial11 scaling rotating and moving using pipeline object
    to encapsulate world transformation matrix

--- main.cpp
+++ main.cpp
@@ -5,11 +5,15 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 
+#include "ogldev_pipeline.h"
 #include "ogldev_math_3d.h"
 
 GLuint VBO;
 GLuint IBO;
 GLuint gWorldLocation;
+
+const char* pVSFileName = "shader.vs";
+const char* pFSFileName = "shader.fs";
 
 static void RenderSceneCB()
 {
@@ -19,14 +23,12 @@ static void RenderSceneCB()
 
     Scale += 0.001f;
 
-    Matrix4f World;
+    Pipeline p;
+    p.Scale(sinf(Scale * 0.1f), sinf(Scale * 0.1f), sinf(Scale * 0.1f));
+    p.WorldPos(sinf(Scale), 0.0f, 0.0f);
+    p.Rotate(sinf(Scale) * 90.0f, sinf(Scale) * 90.0f, sinf(Scale) * 90.0f);
 
-    World.m[0][0] = cosf(Scale); World.m[0][1] = 0.0f; World.m[0][2] = -sinf(Scale); World.m[0][3] = 0.0f;
-    World.m[1][0] = 0.0;         World.m[1][1] = 1.0f; World.m[1][2] = 0.0f        ; World.m[1][3] = 0.0f;
-    World.m[2][0] = sinf(Scale); World.m[2][1] = 0.0f; World.m[2][2] = cosf(Scale) ; World.m[2][3] = 0.0f;
-    World.m[3][0] = 0.0f;        World.m[3][1] = 0.0f; World.m[3][2] = 0.0f        ; World.m[3][3] = 1.0f;
-
-    glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &World.m[0][0]);
+    glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, (const GLfloat*)p.GetWorldTrans());
 
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -36,8 +38,6 @@ static void RenderSceneCB()
     glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
 
     glDisableVertexAttribArray(0);
-
-    glutPostRedisplay();
 
     glutSwapBuffers();
 }
@@ -79,22 +79,17 @@ static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum Shad
 
     if (ShaderObj == 0) {
         fprintf(stderr, "Error creating shader type %d\n", ShaderType);
-        exit(0);
+        exit(1);
     }
 
     const GLchar* p[1];
     p[0] = pShaderText;
-
     GLint Lengths[1];
-    Lengths[0] = (GLint)strlen(pShaderText);
-
+    Lengths[0] = strlen(pShaderText);
     glShaderSource(ShaderObj, 1, p, Lengths);
-
     glCompileShader(ShaderObj);
-
     GLint success;
     glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
-
     if (!success) {
         GLchar InfoLog[1024];
         glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
@@ -105,9 +100,6 @@ static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum Shad
     glAttachShader(ShaderProgram, ShaderObj);
 }
 
-const char* pVSFileName = "shader.vs";
-const char* pFSFileName = "shader.fs";
-
 static void CompileShaders()
 {
     GLuint ShaderProgram = glCreateProgram();
@@ -117,34 +109,29 @@ static void CompileShaders()
         exit(1);
     }
 
-    std::string vs, fs;
+    string vs, fs;
 
     if (!ReadFile(pVSFileName, vs)) {
         exit(1);
     };
 
-    AddShader(ShaderProgram, vs.c_str(), GL_VERTEX_SHADER);
-
     if (!ReadFile(pFSFileName, fs)) {
         exit(1);
     };
 
+    AddShader(ShaderProgram, vs.c_str(), GL_VERTEX_SHADER);
     AddShader(ShaderProgram, fs.c_str(), GL_FRAGMENT_SHADER);
 
     GLint Success = 0;
     GLchar ErrorLog[1024] = { 0 };
 
     glLinkProgram(ShaderProgram);
-
     glGetProgramiv(ShaderProgram, GL_LINK_STATUS, &Success);
     if (Success == 0) {
         glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
         fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
         exit(1);
     }
-
-    gWorldLocation = glGetUniformLocation(ShaderProgram, "gWorld");
-    assert(gWorldLocation != 0xFFFFFFFF);
 
     glValidateProgram(ShaderProgram);
     glGetProgramiv(ShaderProgram, GL_VALIDATE_STATUS, &Success);
@@ -155,6 +142,9 @@ static void CompileShaders()
     }
 
     glUseProgram(ShaderProgram);
+
+    gWorldLocation = glGetUniformLocation(ShaderProgram, "gWorld");
+    assert(gWorldLocation != 0xFFFFFFFF);
 }
 
 int main(int argc, char** argv)
@@ -163,7 +153,7 @@ int main(int argc, char** argv)
     glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH);
     glutInitWindowSize(2560, 1600);
     glutInitWindowPosition(0, 0);
-    glutCreateWindow("Tutorial 10");
+    glutCreateWindow("Tutorial 11");
 
     InitializeGlutCallbacks();
 

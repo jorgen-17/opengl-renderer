b65c54b19343ba211cad405fb191ef4ee9f2fa66
jorgen-17 <jorgenonelljr@gmail.com>
2026-03-17 23:29:33 -0400

    tweeking build file

--- build.sh
+++ build.sh
@@ -7,7 +7,8 @@ if [ -v $ROOTDIR ]; then
 
 CC=g++
 OGL_CPPFLAGS="$CPPFLAGS -I$ROOTDIR/include -std=c++20"
-OGL_LDFLAGS=`pkg-config --libs glew`
-OGL_LDFLAGS="$OGL_LDFLAGS -lglut"
+GLEW_LDFLAGS=`pkg-config --libs glew`
+GLUT_LDFLAGS=`pkg-config --libs glut`
+OGL_LDFLAGS="$GLEW_LDFLAGS $GLUT_LDFLAGS"
 
 $CC main.cpp $ROOTDIR/common/ogldev_util.cpp $ROOTDIR/common/pipeline.cpp $ROOTDIR/common/math_3d.cpp $OGL_CPPFLAGS $OGL_LDFLAGS -o main

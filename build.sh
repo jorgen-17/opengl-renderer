62f0338a89fa3fa592c4959b866a2a48d698925d
jorgen-17 <jorgenonelljr@gmail.com>
2026-03-30 21:37:22 -0400

    adding -g compile flag to add debug symbols, also debugging via lldb nvim dap :D

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ OGL_CPPFLAGS="$CPPFLAGS -I./include -I./common/FreetypeGL -std=c++23"
 OGL_LDFLAGS=`pkg-config --libs glew ImageMagick++ freetype2 glut`
 FILES_2_COMPILE="lighting_technique.cpp ./common/ogldev_util.cpp ./common/pipeline.cpp ./common/math_3d.cpp ./common/camera.cpp ./common/ogldev_atb.cpp ./common/glut_backend.cpp ./common/ogldev_texture.cpp ./common/ogldev_basic_lighting.cpp ./common/technique.cpp ./common/ogldev_app.cpp ./common/FreetypeGL/freetypeGL.cpp ./common/3rdparty/stb_image.cpp"
 
-$CC main.cpp $FILES_2_COMPILE $OGL_CPPFLAGS $OGL_LDFLAGS -o main
+$CC -g main.cpp $FILES_2_COMPILE $OGL_CPPFLAGS $OGL_LDFLAGS -o main

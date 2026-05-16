17ec09ea1d02e145029053ca3ea14f850ec7fe5b
jorgen-17 <jorgenonelljr@gmail.com>
2026-05-15 23:52:49 -0400

    tutorial32 dragging objects around with mouse

--- main.cpp
+++ main.cpp
@@ -22,11 +22,11 @@ static void KeyCallback(GLFWwindow* window, int key, int scancode, int action, i
 static void CursorPosCallback(GLFWwindow* window, double x, double y);
 static void MouseButtonCallback(GLFWwindow* window, int Button, int Action, int Mode);
 
-class Tutorial31
+class Tutorial32
 {
 public:
 
-    Tutorial31()
+    Tutorial32()
     {
         m_directionalLight.Color = Vector3f(1.0f, 1.0f, 1.0f);
         m_directionalLight.AmbientIntensity = 3.0f;
@@ -39,7 +39,7 @@ public:
         m_worldPos[2] = Vector3f(0.0f, 2.0f, 20.0f);
     }
 
-    virtual ~Tutorial31()
+    virtual ~Tutorial32()
     {
         SAFE_DELETE(m_pGameCamera);
         SAFE_DELETE(pMesh);
@@ -69,7 +69,9 @@ public:
 
     void RenderSceneCB()
     {
-        m_pGameCamera->OnRender();
+        if (m_mobileCamera) {
+            m_pGameCamera->OnRender();
+        }
 
         // There's no point in the picking phase when the mouse is not pressed
         if (m_leftMouseButton.IsPressed) {
@@ -108,31 +110,75 @@ public:
     {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        WorldTrans& worldTransform = pMesh->GetWorldTransform();
-        Matrix4f View = m_pGameCamera->GetMatrix();
-        Matrix4f Projection = m_pGameCamera->GetProjectionMat();
-
-        // If the left mouse button is clicked check if it hit a triangle
-        // and color it red
-        int clicked_object_id = -1;
         if (m_leftMouseButton.IsPressed) {
+            HandleMouseButtonPressed();
+        } else {
+            m_clicked_object_id = -1;
+        }
+
+        RenderObjects();
+
+    }
+
+    void HandleMouseButtonPressed()
+    {
+        Matrix4f View = m_pGameCamera->GetMatrix();
+
+        if (m_leftMouseButton.FirstTime) {
             PickingTexture::PixelInfo Pixel = m_pickingTexture.ReadPixel(m_leftMouseButton.x, WINDOW_HEIGHT - m_leftMouseButton.y - 1);
 
             if (Pixel.ObjectID != 0) {
-                // Compensate for the SetObjectindex call in the picking phase
-                clicked_object_id = Pixel.ObjectID - 1;
-                assert(clicked_object_id < ARRAY_SIZE_IN_ELEMENTS(m_worldPos));
-                m_simpleColorEffect.Enable();
-                worldTransform.SetPosition(m_worldPos[clicked_object_id]);
-                Matrix4f World = worldTransform.GetMatrix();
-                Matrix4f WVP = Projection * View * World;
-                m_simpleColorEffect.SetWVP(WVP);
-                pMesh->Render(Pixel.DrawID, Pixel.PrimID);
+                // Compensate for the SetObjectIndex call in the picking phase
+                m_clicked_object_id = Pixel.ObjectID - 1;
+                assert(m_clicked_object_id < ARRAY_SIZE_IN_ELEMENTS(m_worldPos));
+                m_objViewSpacePos = View * Vector4f(m_worldPos[m_clicked_object_id], 1.0f);
+                m_leftMouseButton.FirstTime = false;
             }
         }
 
+        DragTheObject();
+    }
+
+    void DragTheObject()
+    {
+        Matrix4f Projection = m_pGameCamera->GetProjectionMat();
+        Matrix4f ProjectionInv = Projection.Inverse();
+
+        // Step 1 - Viewport to NDC
+        float mouse_x = (float)m_leftMouseButton.x;
+        float mouse_y = (float)m_leftMouseButton.y;
+
+        float ndc_x = (2.0f * mouse_x) / WINDOW_WIDTH - 1.0f;
+        float ndc_y = 1.0f - (2.0f * mouse_y) / WINDOW_HEIGHT; // flip the Y axis
+
+        // Step 2 - NDC to view (my version)
+        float FOV = m_pGameCamera->GetPersProjInfo().FOV;
+        float focal_length = 1.0f/tanf(ToRadian(FOV / 2.0f));
+        float ar = (float)WINDOW_HEIGHT / (float)WINDOW_WIDTH;
+        Vector3f ray_view(ndc_x / focal_length, (ndc_y * ar) / focal_length, 1.0f);
+
+        // Step 2 - NDC to view (Anton's version)
+        Vector4f ray_ndc_4d(ndc_x, ndc_y, 1.0f, 1.0f);
+        Vector4f ray_view_4d = ProjectionInv * ray_ndc_4d;
+
+        // Step 3 - intersect view vector with object Z plane (in view)
+        Vector4f view_space_intersect = Vector4f(ray_view * m_objViewSpacePos.z, 1.0f);
+
+        // Step 4 - View to World space
+        Matrix4f View = m_pGameCamera->GetMatrix();
+        Matrix4f InvView = View.Inverse();
+        Vector4f point_world = InvView * view_space_intersect;
+        m_worldPos[m_clicked_object_id] = Vector3f(point_world);
+    }
+
+    void RenderObjects()
+    {
         // render the objects as usual
         m_lightingEffect.Enable();
+
+        WorldTrans& worldTransform = pMesh->GetWorldTransform();
+        Matrix4f View = m_pGameCamera->GetMatrix();
+        Matrix4f Projection = m_pGameCamera->GetProjectionMat();
 
         for (unsigned int i = 0 ; i < ARRAY_SIZE_IN_ELEMENTS(m_worldPos) ; i++) {
             worldTransform.SetPosition(m_worldPos[i]);
@@ -144,10 +190,10 @@ public:
             m_directionalLight.CalcLocalDirection(worldTransform);
             m_lightingEffect.SetDirectionalLight(m_directionalLight);
 
-            if (i == clicked_object_id) {
-                 m_lightingEffect.SetColorMod(Vector4f(0.0f, 1.0, 0.0, 1.0f));
+            if (i == m_clicked_object_id) {
+                m_lightingEffect.SetColorMod(Vector4f(0.0f, 1.0, 0.0, 1.0f));
             } else {
-                 m_lightingEffect.SetColorMod(Vector4f(1.0f, 1.0, 1.0, 1.0f));
+                m_lightingEffect.SetColorMod(Vector4f(1.0f, 1.0, 1.0, 1.0f));
             }
 
             pMesh->Render(NULL);
@@ -163,6 +209,27 @@ public:
             glfwTerminate();
             exit(0);
             break;
+        case ' ':
+            if (state == GLFW_PRESS) {
+                m_mobileCamera = !m_mobileCamera;
+            }
+            break;
+
+        case 'a':
+            m_directionalLight.AmbientIntensity += 0.05f;
+            break;
+
+        case 's':
+            m_directionalLight.AmbientIntensity -= 0.05f;
+            break;
+
+        case 'z':
+            m_directionalLight.DiffuseIntensity += 0.05f;
+            break;
+
+        case 'x':
+            m_directionalLight.DiffuseIntensity -= 0.05f;
+            break;
         default:
             m_pGameCamera->OnKeyboard(key);
         }
@@ -170,16 +237,31 @@ public:
 
     void PassiveMouseCB(int x, int y)
     {
-        m_pGameCamera->OnMouse(x, y);
+        if (m_mobileCamera) {
+            m_pGameCamera->OnMouse(x, y);
+        } else {
+            // avoid a camera glitch when it becomes mobile again
+            m_pGameCamera->UpdateMousePosSilent(x, y);
+        }
+
+        if (m_leftMouseButton.IsPressed) {
+            m_leftMouseButton.x = x;
+            m_leftMouseButton.y = y;
+        }
     }
 
     void MouseCB(int button, int action, int x, int y)
     {
         if (button == GLFW_MOUSE_BUTTON_LEFT) {
             m_leftMouseButton.IsPressed = (action == GLFW_PRESS);
-            m_leftMouseButton.x = x;
-            m_leftMouseButton.y = y;
+
+            if (!m_leftMouseButton.IsPressed) {
+                m_leftMouseButton.FirstTime = true;
+            }
         }
+
+        m_leftMouseButton.x = x;
+        m_leftMouseButton.y = y;
     }
 
 private:
@@ -188,7 +270,9 @@ private:
     {
         int major_ver = 0;
         int minor_ver = 0;
-        window = glfw_init(major_ver, minor_ver, WINDOW_WIDTH, WINDOW_HEIGHT, fullscreen, "Tutorial 31");
+        window = glfw_init(major_ver, minor_ver, WINDOW_WIDTH, WINDOW_HEIGHT, fullscreen, "Tutorial 32");
+
+        glfwSetCursorPos(window, WINDOW_WIDTH / 2.0, WINDOW_HEIGHT / 2.0);
     }
 
     void InitCallbacks()
@@ -239,11 +323,9 @@ private:
     {
         pMesh = new BasicMesh();
 
-        pMesh->LoadMesh("./content/spider.obj");
+        pMesh->LoadMesh("./content/box.obj");
 
-        WorldTrans& worldTransform = pMesh->GetWorldTransform();
-        worldTransform.SetScale(0.1f);
-        worldTransform.SetRotation(0.0f, 90.0f, 0.0f);
+        pMesh->GetWorldTransform();
     }
 
     GLFWwindow* window = NULL;
@@ -251,19 +333,25 @@ private:
     PickingTechnique m_pickingEffect;
     SimpleColorTechnique m_simpleColorEffect;
     BasicCamera* m_pGameCamera = NULL;
+    bool m_mobileCamera = false;
     DirectionalLight m_directionalLight;
     BasicMesh* pMesh = NULL;
     PickingTexture m_pickingTexture;
     struct {
         bool IsPressed = false;
+        bool FirstTime = true;
         int x;
         int y;
     } m_leftMouseButton;
     Vector3f m_worldPos[3];
     PersProjInfo persProjInfo;
+    Vector4f m_objViewSpacePos;
+    Vector3f m_intersectionPoint;
+    Vector3f m_translation;
+    int m_clicked_object_id = -1;
 };
 
-Tutorial31* app = NULL;
+Tutorial32* app = NULL;
 
 static void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
@@ -286,7 +374,7 @@ static void MouseButtonCallback(GLFWwindow* window, int Button, int Action, int 
 
 int main(int argc, char** argv)
 {
-    app = new Tutorial31();
+    app = new Tutorial32();
 
     app->Init();
 
